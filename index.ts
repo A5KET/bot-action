7ca54ab406c7af893b7c55f60bba380ed0f8cf5a
A5KET <asketinin@gmail.com>
2025-03-29 18:23:44 +0200

    Version 0.0.8

--- index.ts
+++ index.ts
@@ -11,7 +11,7 @@ export interface ActionBuilderOptions {
 
 
 export interface ActionBuilderState<P extends Record<string, any>> {
-    readonly names: string[]
+    readonly names: readonly string[]
     readonly params: { [K in keyof P]: ActionBuilderParam<P[K]> }
     readonly isAnyNames: boolean
     readonly isAnyParams: boolean
